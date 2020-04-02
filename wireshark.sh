a0eb2fe7043b592f12254206f6cfc4b531160001
Michael Power <powerm3@tcd.ie>
2020-04-02 16:24:12 +0100

    Update wireshark.sh

--- wireshark.sh
+++ wireshark.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Here we are going to run wireshark in monitor mode (-I), outputting the packet tree and Hex/ASCII dump
+# Here we are going to run wireshark, outputting the packet tree and Hex/ASCII dump
 # After restart network manager to get our signal back up
-tshark -i wlan0 -x -V -I
-service network-manager restart
+tshark -i wlan0 -x -V > test.txt
