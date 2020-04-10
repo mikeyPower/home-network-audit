1efb14984ef9bf5ef9d18bd5a4372b1b311c805f
mikeyPower <powerm3@tcd.ie>
2020-04-10 15:23:50 +0100

    found ip address of network

--- nmap.sh
+++ nmap.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-# Run NMap simply looking for active host while disabling both port scans (-sn) and dns resolution (-n)
+# Get ip address information of current network
+ip_address="$(ip -f inet addr show wlan0)"
+# Second sed removes everything up until the first space
+ip_range="$(echo "$ip_address" | sed -n 's/^ *inet \([.0-9]\)/\1/p' |  sed 's/\s.*$//')"
+echo $ip_range
+# Get current timestamp
 timestamp=$(date +%s)
-nmap -sn -n 111.111.111.0/24 -oX nmap_data/nmap_"${timestamp}".xml
+# Run NMap simply looking for active host while disabling both port scans (-sn) and dns resolution (-n)
+nmap -sn -n "${ip_range}" -oX nmap_data/nmap_"${timestamp}".xml
