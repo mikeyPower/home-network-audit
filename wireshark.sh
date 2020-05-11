19c0f16c1b8756a98e8bd291ea646a9466a196ed
Michael Power <powerm3@tcd.ie>
2020-05-11 22:16:47 +0100

    Update wireshark.sh

--- wireshark.sh
+++ wireshark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Here we are going to run wireshark, outputting the result to file test
+#Here we are going to run wireshark filtering (-f) only for traffic over http while running tshark in monitor mode
+#outputting the result to file to wireshark_data folder with stopping after the file reaches a file size (-a filesize:) of 10000KB(10MB)
 timestamp=$(date +%s)
-tshark -i wlan0 -f 'port http' -I -w wireshark_data/wireshark_"${timestamp}".pcapng 
+tshark -i wlan0 -f 'port http' -I -a filesize:10000 -w wireshark_data/wireshark_"${timestamp}".pcapng 
